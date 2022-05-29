a2d3ae560402467633aaa7fb6d5cbc97fcd084ca
Anik Hossain <anik.wdev@gmail.com>
2022-05-29 18:54:07 +0600

    day-4

---
+++ day-4/4-nCr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int factorial = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int n, r;
+    cin >> n >> r;
+    int result = fact(n) / (fact(r) * fact(n - r));
+    cout << result << endl;
+    return 0;
+}
