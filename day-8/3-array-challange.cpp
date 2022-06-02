57b6c7c6a82861bfda4d8f262555e206e7456db2
Anik Hossain <anik.wdev@gmail.com>
2022-06-02 20:32:58 +0600

    day-8 (array challange)

---
+++ day-8/3-array-challange.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int result = 2;
+    int pd = a[1] - a[0];
+    int j = 2;
+    int curr = 2;
+    while (j < n)
+    {
+        if (pd == a[j] - a[j - 1])
+            curr++;
+        else
+            pd = a[j] - a[j - 1], curr = 2;
+        result = max(result, curr);
+        j++;
+    }
+    cout << result << endl;
+    return 0;
+}
